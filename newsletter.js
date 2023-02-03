86bdec41c34dd4d0ccc7d637896db250b314cf39
syedshaon <syedshaon99@gmail.com>
2023-02-03 21:46:04 +0600

    some changes done

--- newsletter.js
+++ newsletter.js
@@ -9,8 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public")); 
 
-// app.listen( 3000, () => {
-app.listen( process.env.PORT || 3000, () => {
+app.listen( 3000, () => {
     console.log("server is listening at 3000");
 });
 
